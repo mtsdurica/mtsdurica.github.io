825b652e54e53c328e488b83d10e8966fc50a6ba
Matus Durica <mtsdurica@gmail.com>
2024-04-15 18:46:26 +0200

    .

--- main.js
+++ main.js
@@ -4,4 +4,11 @@ const navContent = document.getElementById("nav-content");
 navToggle.addEventListener("click", () => {
     navToggle.classList.toggle("active");
     navContent.classList.toggle("active");
-})
+});
+
+const aboutNav = document.getElementById("about-href");
+const aboutSection = document.getElementById("about-section");
+
+aboutNav.addEventListener("click", () => {
+    aboutSection.scrollIntoView({ behavior: "smooth" });
+});
